9935bcca7b68fd9ccd6dd3f30550f20bc4c9c6f1
Jonathan Gall <odajay@gmail.com>
2017-07-14 00:47:48 +0200

    Starting implemantation of mvc with knockout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,24 +1,40 @@
-var restaurantViewModel = [{
-    name: '77 Sushi',
-    lat: 50.0581635,
-    lng: 19.9389086
-}, {
-    name: 'Youmiko Sushi',
-    lat: 50.0504222,
-    lng: 19.943054
-}, {
-    name: 'Zen Sushi',
-    lat: 50.0621398,
-    lng: 19.9417126
-}, {
-    name: 'Urara',
-    lat: 50.0643265802915,
-    lng: 19.9408391802915
-}, {
-    name: 'Youmiko Sushi',
-    lat: 50.0569154,
-    lng: 19.9324689
-}, ]
+var restaurantModel = {
+    currentArray: [{
+        name: '77 Sushi',
+        lat: 50.0581635,
+        lng: 19.9389086
+    }, {
+        name: 'Youmiko Sushi',
+        lat: 50.0504222,
+        lng: 19.943054
+    }, {
+        name: 'Zen Sushi',
+        lat: 50.0621398,
+        lng: 19.9417126
+    }, {
+        name: 'Urara',
+        lat: 50.0643265802915,
+        lng: 19.9408391802915
+    }, {
+        name: 'Youmiko Sushi',
+        lat: 50.0569154,
+        lng: 19.9324689
+    }],
+
+    filteredArray: [],
+}
 
 
-ko.applyBindings(restaurantViewModel);
+
+var viewModel = {
+    currentRestaurant: ko.observableArray(restaurantModel.currentArray);
+    // var self = this;
+    // self.array = restaurantModel.currentArray;
+    // self.restoFilter = ko.observable();
+
+    // self.doFilter = function() {
+    //     alert("test");
+    // }
+};
+
+ko.applyBindings(viewModel);
