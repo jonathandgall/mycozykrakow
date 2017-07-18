024dcf15f553ba15f67ff8b6d75bc2035a0753d6
Jonathan Gall <odajay@gmail.com>
2017-07-18 11:31:39 +0200

    Implementing MVVM logic with dynamic search

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,40 +1,49 @@
 var restaurantModel = {
-    currentArray: [{
-        name: '77 Sushi',
-        lat: 50.0581635,
-        lng: 19.9389086
-    }, {
-        name: 'Youmiko Sushi',
-        lat: 50.0504222,
-        lng: 19.943054
-    }, {
-        name: 'Zen Sushi',
-        lat: 50.0621398,
-        lng: 19.9417126
-    }, {
-        name: 'Urara',
-        lat: 50.0643265802915,
-        lng: 19.9408391802915
-    }, {
-        name: 'Youmiko Sushi',
-        lat: 50.0569154,
-        lng: 19.9324689
-    }],
+  currentArray: [{
+    name: '77 Sushi',
+    lat: 50.0581635,
+    lng: 19.9389086
+  }, {
+    name: 'Youmiko Sushi',
+    lat: 50.0504222,
+    lng: 19.943054
+  }, {
+    name: 'Zen Sushi',
+    lat: 50.0621398,
+    lng: 19.9417126
+  }, {
+    name: 'Urara',
+    lat: 50.0643265802915,
+    lng: 19.9408391802915
+  }, {
+    name: 'Youmiko Sushi',
+    lat: 50.0569154,
+    lng: 19.9324689
+  }],
+  // filteredArray: [{ name: 'Youmiko Sushi' }]
 
-    filteredArray: [],
 }
 
 
-
 var viewModel = {
-    currentRestaurant: ko.observableArray(restaurantModel.currentArray);
-    // var self = this;
-    // self.array = restaurantModel.currentArray;
-    // self.restoFilter = ko.observable();
+  restaurants: ko.observableArray([]),
+  query: ko.observable(''),
 
-    // self.doFilter = function() {
-    //     alert("test");
-    // }
+  search: function(arg) {
+    //removing current restaurants at the beginning of the loop
+    viewModel.restaurants.removeAll();
+    //check if value of arg exist in our model
+    for (var i = 0; i < restaurantModel.currentArray.length; i++) {
+      if (restaurantModel.currentArray[i].name.toLowerCase().indexOf(arg.toLowerCase()) >= 0) {
+        viewModel.restaurants.push(restaurantModel.currentArray[i])
+      }
+    }
+  console.log(arg)}
 };
+
+
+
+viewModel.query.subscribe(viewModel.search);
+
 
 ko.applyBindings(viewModel);
