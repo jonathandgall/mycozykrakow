eab8d873cb67fb973633d96fbb410dcaba28f318
Jonathan Gall <odajay@gmail.com>
2017-07-18 19:01:23 +0200

    Moving the map logic in viewModel

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,37 +16,70 @@ var restaurantModel = {
     lat: 50.0643265802915,
     lng: 19.9408391802915
   }, {
-    name: 'Youmiko Sushi',
-    lat: 50.0569154,
-    lng: 19.9324689
+    name: 'Edo Sushi Bar',
+    lat: 50.0521837,
+    lng: 19.9429686
   }],
 
   //
 }
 
+var sushiRenders = [];
+
+
+var map;
+
+function initMap() {
+  // Constructor creates a new map - only center and zoom are required.
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: 50.06193,
+      lng: 19.94
+    },
+    zoom: 13
+  });
+
+
+  for (var i = 0; i < restaurantModel.currentArray.length ; i++) {
+
+    sushiRenders[i] = new google.maps.Marker({
+      map: map,
+      position: {
+        lat: restaurantModel.currentArray[i].lat,
+        lng: restaurantModel.currentArray[i].lng
+      },
+      title: restaurantModel.currentArray[i].name
+    });
+  }
+  // console.log(sushiRenders)
+}
+
 
 var viewModel = {
-    //exporting a copy of the current array so that it is shown as a list on my landing page
-    restaurants: ko.observableArray(restaurantModel.currentArray.slice(0, restaurantModel.currentArray.length)),
+  //exporting a copy of the current array so that it is shown as a list on my landing page
+  restaurants: ko.observableArray(restaurantModel.currentArray.slice(0, restaurantModel.currentArray.length)),
 
-      query: ko.observable(''),
+  query: ko.observable(''),
 
-      search: function(arg) {
-        //removing current restaurants at the beginning of the loop
-        viewModel.restaurants.removeAll();
-        //check if value of arg exist in our model
-        for (var i = 0; i < restaurantModel.currentArray.length; i++) {
-          if (restaurantModel.currentArray[i].name.toLowerCase().indexOf(arg.toLowerCase()) >= 0) {
-            viewModel.restaurants.push(restaurantModel.currentArray[i])
-          }
-        }
-        console.log(arg)
+  search: function(arg) {
+    //removing current restaurants at the beginning of the loop
+    viewModel.restaurants.removeAll();
+    //check if value of arg exist in our model
+    for (var i = 0; i < restaurantModel.currentArray.length; i++) {
+      if (restaurantModel.currentArray[i].name.toLowerCase().indexOf(arg.toLowerCase()) >= 0) {
+        viewModel.restaurants.push(restaurantModel.currentArray[i])
+      } else {
+        sushiRenders[i].setMap(null)
       }
-    };
+
+    }
+    console.log(arg)
+  }
+};
 
 
 
-    viewModel.query.subscribe(viewModel.search);
+viewModel.query.subscribe(viewModel.search);
 
 
-    ko.applyBindings(viewModel);
+ko.applyBindings(viewModel);
