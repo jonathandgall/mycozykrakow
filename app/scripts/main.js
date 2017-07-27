54c111587f3afedc8d0d5bd1c907a721c37f3b98
Jonathan Gall <odajay@gmail.com>
2017-07-27 17:43:57 +0200

    Providing Foursquare attribution

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,7 @@
 var restaurantModel;
 
 //retrieve in async the model
-$.getJSON('./scripts/restaurant.json', function(data) {
+$.getJSON('/scripts/restaurant.json', function(data) {
     restaurantModel = data;
   })
   //init the application now that i know the view model is ready
@@ -91,7 +91,7 @@ function showInfoWindow(restaurant) {
     .done(function(data) {
       var photoUrl = data.response.photos.items[0].prefix + data.response.photos.items[0].width + 'x' + data.response.photos.items[0].height + data.response.photos.items[0].suffix;
       var infowindow = new google.maps.InfoWindow({
-        content: '<p class="infowindow-render">A picture from inside</p><p><img src="' + photoUrl + '" alt="Pictures" width="100px"></p>'
+        content: '<p class="infowindow-render">A picture from inside <br>Provided Foursquare API</p><p><img src="' + photoUrl + '" alt="Pictures" width="100px"></p>'
       });
       //clear existing infowindows before showing the next one
       clearInfoWindow();
